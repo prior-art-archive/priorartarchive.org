28ff898772e8e8c07156d93b05f91f1963426173
Travis Rich <travis.s.rich@gmail.com>
2018-12-11 16:13:05 -0500

    dev: Fix opensearch static serve

--- server/server.js
+++ server/server.js
@@ -85,6 +85,7 @@ app.use('/service-worker.js', express.static('static/service-worker.js'));
 app.use('/favicon.png', express.static('static/favicon.png'));
 app.use('/favicon.ico', express.static('static/favicon.png'));
 app.use('/robots.txt', express.static('static/robots.txt'));
+app.use('/opensearch.xml', express.static('static/opensearch.xml'));
 
 
 /* ------------- */
