54a092420a4c58da42c0b3aef80bf49fc2b76b3a
Joel Gustafson <joelg@mit.edu>
2019-02-14 15:05:56 -0500

    fix: additional query payload parsing to transform terms within span queries into span_terms, and better error handling

--- server/apiRoutes/search.js
+++ server/apiRoutes/search.js
@@ -18,97 +18,123 @@ const operatorSet = new Set(operators);
 
 const getOrganizationBySlug = slug => Organization.findOne({ where: { slug } });
 
+const spanQueries = ['span_term', 'span_multi', 'span_near', 'span_or'];
+
 app.post('/api/search', (req, res)=> {
 	const { query, operator, offset, fileType, source } = req.body;
 	if (query && operatorSet.has(operator)) {
 		lambda.invoke({
 			FunctionName: 'queryParser',
 			Payload: JSON.stringify({ query, operator })
-		}, async (err, data) => {
-			if (data && data.StatusCode === 200) {
-				const payload = JSON.parse(JSON.parse(data.Payload));
-				if (fileType.length || source.length) {
-					if (!payload.query.bool.filter) payload.query.bool.filter = [];
-					if (fileType.length) {
-						payload.query.bool.filter.push({
-							terms: { contentType: fileType }
-						});
-					}
-					if (source.length) {
-						const orgs = await Promise.all(source.map(getOrganizationBySlug));
-						payload.query.bool.filter.push({
-							terms: { organizationId: orgs.map(org => org.id) }
-						});
-					}
-				}
-
-				const response = await client.search({
-					index: process.env.ELASTIC_INDEX,
-					body: {
-						_source: { excludes: 'text' },
-						from: offset || 0,
-						size: 10,
-						highlight: { fields: { text: { } } },
-						aggs: {
-							source: { terms: { field: 'organizationId' } },
-							fileType: { terms: { field: 'contentType' } },
-							dateRange: {
-								auto_date_histogram: {
-									field: 'publicationDate',
-									buckets: 5
-								}
-							}
-						},
-						// sort: { [req.body.sort]: 'asc' },
-						...payload
-					}
-				});
-
-				if (response.timed_out) res.status(504).end();
-				else {
-					// There will probably be several results with the same organizationId;
-					// instead of looking them all we'll make a map of ids to names ourselves.
-					// This might be unnecessary if sequelize caches id lookups?
-					const names = {};
-					const hits = await Promise.all(response.hits.hits.map(hit => {
-						const { _id: id, _score: score, highlight } = hit;
-						const { organizationId, ...rest } = hit._source;
-						if (names[organizationId] === undefined) {
-							names[organizationId] = Organization.findByPk(organizationId);
+		}, (err, data) => {
+			new Promise(async (resolve, reject) => {
+				if (data && data.StatusCode === 200) {
+					const payload = JSON.parse(JSON.parse(data.Payload));
+					if (fileType.length || source.length) {
+						if (!payload.query.bool.filter) payload.query.bool.filter = [];
+						if (fileType.length) {
+							payload.query.bool.filter.push({
+								terms: { contentType: fileType }
+							});
 						}
-						return names[organizationId].then(org => ({
-							id,
-							score,
-							sourceSlug: org.slug,
-							sourceName: org.name,
-							highlight,
-							...rest
-						}));
-					}));
+						if (source.length) {
+							const orgs = await Promise.all(source.map(getOrganizationBySlug)).catch(reject);
+							if (!orgs) return;
+							payload.query.bool.filter.push({
+								terms: { organizationId: orgs.map(org => org.id) }
+							});
+						}
+					}
 
-					const { source: { buckets, ...rest } } = response.aggregations;
-					const namedBuckets = await Promise.all(buckets.map(item => {
-						const { key, doc_count: count } = item;
-						if (names[key] === undefined) names[key] = Organization.findByPk(key);
-						return names[key].then(org => ({ name: org.name, slug: org.slug, count }));
-					}));
+					// We need to write a new query parser ASAP.
+					if (Array.isArray(payload.query.bool.must)) {
+						payload.query.bool.must.forEach((term, i) => {
+							spanQueries.forEach(span => {
+								if (term[span] && typeof term[span] === 'object') {
+									if (Array.isArray(term[span].clauses)) {
+										term[span].clauses.forEach((clause, j) => {
+											if (typeof clause.term === 'object') {
+												payload.query.bool.must[i][span].clauses[j].span_term = clause.term;
+												delete payload.query.bool.must[i][span].clauses[j].term;
+											}
+										});
+									}
+								}
+							});
+						});
+					}
 
-					res.status(200).json({
-						query: query,
-						operator: operator,
-						aggregations: {
-							dateRange: response.aggregations.dateRange,
-							fileType: response.aggregations.fileType,
-							source: { buckets: namedBuckets, ...rest }
-						},
-						total: response.hits.total,
-						max_score: response.hits.max_score,
-						hits
-					});
+					const response = await client.search({
+						index: process.env.ELASTIC_INDEX,
+						body: {
+							_source: { excludes: 'text' },
+							from: offset || 0,
+							size: 10,
+							highlight: { fields: { text: { } } },
+							aggs: {
+								source: { terms: { field: 'organizationId' } },
+								fileType: { terms: { field: 'contentType' } },
+								dateRange: {
+									auto_date_histogram: {
+										field: 'publicationDate',
+										buckets: 5
+									}
+								}
+							},
+							// sort: { [req.body.sort]: 'asc' },
+							...payload
+						}
+					}).catch(reject);
+					if (!response) return;
+
+					if (response.timed_out) res.status(504).end();
+					else {
+						// There will probably be several results with the same organizationId;
+						// instead of looking them all we'll make a map of ids to names ourselves.
+						// This might be unnecessary if sequelize caches id lookups?
+						const names = {};
+						const hits = await Promise.all(response.hits.hits.map(hit => {
+							const { _id: id, _score: score, highlight } = hit;
+							const { organizationId, ...rest } = hit._source;
+							if (names[organizationId] === undefined) {
+								names[organizationId] = Organization.findByPk(organizationId);
+							}
+							return names[organizationId].then(org => ({
+								id,
+								score,
+								sourceSlug: org.slug,
+								sourceName: org.name,
+								highlight,
+								...rest
+							}));
+						})).catch(reject);
+						if (!hits) return;
+
+						const { source: { buckets, ...rest } } = response.aggregations;
+						const namedBuckets = await Promise.all(buckets.map(item => {
+							const { key, doc_count: count } = item;
+							if (names[key] === undefined) names[key] = Organization.findByPk(key);
+							return names[key].then(org => ({ name: org.name, slug: org.slug, count }));
+						})).catch(reject);
+						if (!namedBuckets) return;
+
+						resolve({
+							query: query,
+							operator: operator,
+							aggregations: {
+								dateRange: response.aggregations.dateRange,
+								fileType: response.aggregations.fileType,
+								source: { buckets: namedBuckets, ...rest }
+							},
+							total: response.hits.total,
+							max_score: response.hits.max_score,
+							hits
+						});
+					}
+				} else {
+					reject(err);
 				}
-			} else {
-				res.status(400).json(err);
-			}
+			}).then(result => res.status(200).json(result)).catch(error => res.status(400).json(error));
 		});
 	} else {
 		res.status(400).json('Invalid query or operator parameters');
