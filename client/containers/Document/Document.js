b5dbef25e43722c1c23fb28acbbb45a4e8752925
Justin Reese <justin@justinreese.com>
2019-06-20 15:18:52 -0500

    Add ability to edit document titles and descriptions
    Users can now edit document titles and descriptions.
    
    On the document detail page, we gain an Edit button that flips the title and description into text fields. Title is required, description is optional. Users can close out of the edit form without saving if they want.
    
    Closes #18

--- client/containers/Document/Document.js
+++ client/containers/Document/Document.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PageWrapper from 'components/PageWrapper/PageWrapper';
-import { hydrateWrapper, generateDocumentTitle } from 'utilities';
-import { HTMLTable } from '@blueprintjs/core';
+import { apiFetch, generateDocumentTitle, hydrateWrapper } from 'utilities';
+import { HTMLTable, Button } from '@blueprintjs/core';
+import InputField from 'components/InputField/InputField';
+import { pick } from '../../utilities';
 
 require('./document.scss');
 
@@ -13,148 +15,315 @@ const propTypes = {
 	assertionData: PropTypes.array.isRequired,
 };
 
-const placeholderDescription = 'No description available.';
+class Document extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {
+				...props.documentData,
+			},
+			newData: pick(props.documentData, ['id', 'title', 'description']),
+			hasChanged: false,
+			putDocumentIsLoading: false,
+			putDocumentError: undefined,
+		};
+		this.onTitleChange = this.onTitleChange.bind(this);
+		this.onDescriptionChange = this.onDescriptionChange.bind(this);
+		this.handlePutDocument = this.handlePutDocument.bind(this);
+		this.handleEditDocumentDataClick = this.handleEditDocumentDataClick.bind(this);
+		this.handleCancelDocumentDataEditClick = this.handleCancelDocumentDataEditClick.bind(this);
+	}
 
-const gatewayUrl = 'https://dev-gateway.underlay.store/ipfs/';
+	onTitleChange(evt) {
+		const { newData } = this.state;
+		const title = evt.target.value;
+		if (newData.title !== title) {
+			this.setState((prevState) => ({
+				newData: {
+					...prevState.newData,
+					title: title,
+				},
+				hasChanged: true,
+			}));
+		}
+	}
 
-const formatDate = (date) => new Date(date).toISOString().slice(0, 10);
+	onDescriptionChange(evt) {
+		const { newData } = this.state;
+		const description = evt.target.value;
+		if (newData.description !== description) {
+			this.setState((prevState) => ({
+				newData: {
+					...prevState.newData,
+					description: description,
+				},
+				hasChanged: true,
+			}));
+		}
+	}
 
-const tableProps = { bordered: true, condensed: true, striped: true };
+	handlePutDocument(evt) {
+		evt.preventDefault();
 
-const Document = function(props) {
-	const {
-		title,
-		description,
-		fileUrl,
-		language,
-		contentType,
-		organizationName,
-		organizationSlug,
-	} = props.documentData;
-	const createdAt = formatDate(props.documentData.createdAt);
-	const updatedAt = formatDate(props.documentData.updatedAt);
-	const publicationDate = formatDate(props.documentData.publicationDate);
-	const cpcCodes = props.documentData.cpcCodes || [];
-	const generatedTitle = generateDocumentTitle(title);
-	return (
-		<div id="document-container">
-			<PageWrapper loginData={props.loginData} locationData={props.locationData}>
-				<div className="container">
-					<div id="flex-container" className="row">
-						<div className="col-12">
-							<header>
-								<h1 className={generatedTitle.isPlaceholder ? 'placeholder' : ''}>
-									{generatedTitle.title}
-								</h1>
-								<section className={description ? '' : 'placeholder'}>
-									{description || placeholderDescription}
-								</section>
-							</header>
-							<main>
-								<div>
-									<section>
-										<HTMLTable {...tableProps}>
-											<tbody>
-												<tr>
-													<td>Source</td>
-													<td>
-														<a
-															href={`/organization/${organizationSlug}`}
-														>
-															{organizationName}
-														</a>
-													</td>
-												</tr>
-												<tr>
-													<td>Created</td>
-													<td>{createdAt}</td>
-												</tr>
-												<tr>
-													<td>Updated</td>
-													<td>{updatedAt}</td>
-												</tr>
-												<tr>
-													<td>Published</td>
-													<td>{publicationDate}</td>
-												</tr>
-												<tr>
-													<td>Language</td>
-													<td>{language}</td>
-												</tr>
-												<tr>
-													<td>Content Type</td>
-													<td>{contentType}</td>
-												</tr>
-												<tr>
-													<td>CPC Codes</td>
-													<td>
-														<ul>
-															{cpcCodes.map((code) => (
-																<li key={code}>{code}</li>
-															))}
-														</ul>
-													</td>
-												</tr>
-											</tbody>
-										</HTMLTable>
-									</section>
-									<section>
-										<h2>Files</h2>
-										{props.assertionData.map((assertion) => (
-											<HTMLTable key={assertion.cid} {...tableProps}>
+		const { newData } = this.state;
+
+		this.setState({
+			putDocumentIsLoading: true,
+			putDocumentError: undefined,
+		});
+
+		return apiFetch('/api/documents', {
+			method: 'PUT',
+			body: JSON.stringify({
+				document: newData,
+			}),
+		})
+			.then(() => {
+				this.setState((prevState) => ({
+					putDocumentIsLoading: false,
+					isEditingDocument: false,
+					data: {
+						...prevState.data,
+						title: newData.title,
+						description: newData.description,
+					},
+				}));
+			})
+			.catch((err) => {
+				this.setState({
+					putDocumentIsLoading: false,
+					putDocumentError: err,
+				});
+			});
+	}
+
+	handleEditDocumentDataClick() {
+		this.setState({
+			isEditingDocument: true,
+		});
+	}
+
+	handleCancelDocumentDataEditClick() {
+		const { data } = this.state;
+
+		this.setState({
+			newData: pick(data, ['id', 'title', 'description']),
+			hasChanged: false,
+			isEditingDocument: false,
+		});
+	}
+
+	renderDocumentDataHeader() {
+		const { data, isEditingDocument } = this.state;
+
+		const generatedTitle = generateDocumentTitle(data.title);
+		const placeholderDescription = 'No description available.';
+
+		return (
+			<>
+				<Button
+					type="button"
+					className="bp3-button document__edit-button"
+					text="Edit"
+					onClick={this.handleEditDocumentDataClick}
+					disabled={isEditingDocument}
+				/>
+				<h1 className={generatedTitle.isPlaceholder ? 'placeholder' : ''}>
+					{`${generatedTitle.title} `}
+				</h1>
+				<section className={data.description ? '' : 'placeholder'}>
+					{data.description || placeholderDescription}
+				</section>
+			</>
+		);
+	}
+
+	renderDocumentDataEditForm() {
+		const { data, newData, hasChanged, putDocumentError, putDocumentIsLoading } = this.state;
+
+		return (
+			<>
+				<form className="document__edit-form" onSubmit={this.handlePutDocument}>
+					<InputField
+						label="Title"
+						isRequired={true}
+						value={newData.title}
+						onChange={this.onTitleChange}
+					/>
+					<InputField
+						label="Description"
+						value={newData.description}
+						onChange={this.onDescriptionChange}
+					/>
+					<InputField error={putDocumentError && 'Error Saving Details'}>
+						<Button
+							type="submit"
+							className="bp3-button bp3-intent-primary"
+							text="Save changes"
+							disabled={
+								!hasChanged ||
+								!newData.title ||
+								(data.title === newData.title &&
+									data.description === newData.description)
+							}
+							loading={putDocumentIsLoading}
+						/>
+						{` or `}
+						<Button
+							type="button"
+							className="bp3-button"
+							text="Close"
+							onClick={this.handleCancelDocumentDataEditClick}
+						/>
+					</InputField>
+				</form>
+			</>
+		);
+	}
+
+	render() {
+		const {
+			fileUrl,
+			language,
+			contentType,
+			organizationName,
+			organizationSlug,
+		} = this.props.documentData;
+
+		const { isEditingDocument } = this.state;
+
+		const formatDate = (date) => new Date(date).toISOString().slice(0, 10);
+		const createdAt = formatDate(this.props.documentData.createdAt);
+		const updatedAt = formatDate(this.props.documentData.updatedAt);
+		const publicationDate = formatDate(this.props.documentData.publicationDate);
+		const cpcCodes = this.props.documentData.cpcCodes || [];
+
+		const gatewayUrl = 'https://dev-gateway.underlay.store/ipfs/';
+		const tableProps = { bordered: true, condensed: true, striped: true };
+
+		return (
+			<div id="document-container">
+				<PageWrapper
+					loginData={this.props.loginData}
+					locationData={this.props.locationData}
+				>
+					<div className="container">
+						<div id="flex-container" className="row">
+							<div className="col-12">
+								<header>
+									{isEditingDocument
+										? this.renderDocumentDataEditForm()
+										: this.renderDocumentDataHeader()}
+								</header>
+								<main>
+									<div>
+										<section>
+											<HTMLTable {...tableProps}>
 												<tbody>
 													<tr>
-														<td>Uploaded</td>
-														<td>{formatDate(assertion.createdAt)}</td>
-													</tr>
-													<tr>
-														<td>Original file</td>
+														<td>Source</td>
 														<td>
 															<a
-																href={
-																	gatewayUrl + assertion.fileCid
-																}
+																href={`/organization/${organizationSlug}`}
 															>
-																{assertion.fileName}
+																{organizationName}
 															</a>
 														</td>
 													</tr>
 													<tr>
-														<td>Transcript</td>
-														<td>
-															<a
-																href={`${gatewayUrl +
-																	assertion.cid}/transcript.txt`}
-															>
-																transcript.txt
-															</a>
-														</td>
+														<td>Created</td>
+														<td>{createdAt}</td>
 													</tr>
 													<tr>
-														<td>Metadata</td>
+														<td>Updated</td>
+														<td>{updatedAt}</td>
+													</tr>
+													<tr>
+														<td>Published</td>
+														<td>{publicationDate}</td>
+													</tr>
+													<tr>
+														<td>Language</td>
+														<td>{language}</td>
+													</tr>
+													<tr>
+														<td>Content Type</td>
+														<td>{contentType}</td>
+													</tr>
+													<tr>
+														<td>CPC Codes</td>
 														<td>
-															<a
-																href={`${gatewayUrl +
-																	assertion.cid}/metadata.csv`}
-															>
-																metadata.csv
-															</a>
+															<ul>
+																{cpcCodes.map((code) => (
+																	<li key={code}>{code}</li>
+																))}
+															</ul>
 														</td>
 													</tr>
 												</tbody>
 											</HTMLTable>
-										))}
-									</section>
-								</div>
-								<iframe title="preview" allowFullScreen src={fileUrl} />
-							</main>
+										</section>
+										<section>
+											<h2>Files</h2>
+											{this.props.assertionData.map((assertion) => (
+												<HTMLTable key={assertion.cid} {...tableProps}>
+													<tbody>
+														<tr>
+															<td>Uploaded</td>
+															<td>
+																{formatDate(assertion.createdAt)}
+															</td>
+														</tr>
+														<tr>
+															<td>Original file</td>
+															<td>
+																<a
+																	href={`
+																		${gatewayUrl}
+																		${assertion.fileCid}
+																	`}
+																>
+																	{assertion.fileName}
+																</a>
+															</td>
+														</tr>
+														<tr>
+															<td>Transcript</td>
+															<td>
+																<a
+																	href={`${gatewayUrl +
+																		assertion.cid}/transcript.txt`}
+																>
+																	transcript.txt
+																</a>
+															</td>
+														</tr>
+														<tr>
+															<td>Metadata</td>
+															<td>
+																<a
+																	href={`${gatewayUrl +
+																		assertion.cid}/metadata.csv`}
+																>
+																	metadata.csv
+																</a>
+															</td>
+														</tr>
+													</tbody>
+												</HTMLTable>
+											))}
+										</section>
+									</div>
+									<iframe title="preview" allowFullScreen src={fileUrl} />
+								</main>
+							</div>
 						</div>
 					</div>
-				</div>
-			</PageWrapper>
-		</div>
-	);
-};
+				</PageWrapper>
+			</div>
+		);
+	}
+}
 
 Document.propTypes = propTypes;
 export default Document;
